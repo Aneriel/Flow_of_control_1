e96b7ff815f68781bcb77d00f1077f893f69804b
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-10-30 12:25:19 +0100

    #02-Zadanie_poprawne

--- Flow_of_controll_1/Program.cs
+++ Flow_of_controll_1/Program.cs
@@ -1,4 +1,88 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata;
+
 string wejscie = Console.ReadLine();
 int[] dane = Array.ConvertAll<string, int>(wejscie.Split(" "), int.Parse);
+int daneZero = dane[0];
+int daneJeden = dane[1];
+if (dane[0] == dane[1] || daneZero+1 == daneJeden|| daneJeden+1 == daneZero)
+{
+    Console.WriteLine("empty");
+    
+}
+else
+{
+    if (dane[1] - dane[0] <= 11&& dane[0] - dane[1] <= 11)
+    {
 
+    
+        if (dane[0] < dane[1])
+        { 
+            while (dane[0] < dane[1])
+            {
+                
+                if (dane[0] + 2 == dane[1])
+                {
+                     Console.WriteLine($"{dane[0]+1}");
+                     break;
+                }
+                else {    
+                    dane[0]++;
+                  Console.Write($"{dane[0]}, ");
+                }
+            }
+        }   
+        else if (dane[1] < dane[0])
+        {
+            while (dane[1] < dane[0])
+            {
+                
+                if (dane[1] + 2 == dane[0])
+                {
+                     Console.WriteLine($"{dane[1]+1}");
+                     break;
+                }
+                else
+                {
+                    dane[1]++;
+                    Console.Write($"{dane[1]}, ");
+                }   
+            }
+        }
+    }
+    else if(dane[0] < dane[1])
+    {
+        while (dane[0] < daneZero+3)
+        {
+            dane[0]++;
+            Console.Write($"{dane[0]}, ");
+
+        }
+        if (dane[0] == daneZero + 3)
+        {
+            Console.Write("..., ");
+            Console.WriteLine($"{dane[1]-2}, {dane[1]-1}");
+        }
+    }
+    else
+    {
+        while (dane[1] < daneJeden+3)
+        {
+            dane[1]++;
+            Console.Write($"{dane[1]}, ");
+
+        }
+        if (dane[1] == daneJeden + 3)
+        {
+            Console.Write("..., ");
+            Console.WriteLine($"{dane[0]-2}, {dane[0]-1}");
+        }
+    }
+    
+
+}
+#if DEBUG
+    Console.WriteLine("Press enter to close...");
+    Console.ReadLine();
+#endif
+ 
