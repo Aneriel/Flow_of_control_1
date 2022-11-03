b1e605fc7041663e1dc941f0766f34683dd79a3d
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-11-03 13:06:35 +0100

    #03-Posprzatanie_kodu,ogarniecie_dzialania<10

--- Flow_of_controll_1/Program.cs
+++ Flow_of_controll_1/Program.cs
@@ -1,12 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Reflection.Metadata;
+﻿using System.Reflection.Metadata;
 
 string wejscie = Console.ReadLine();
 int[] dane = Array.ConvertAll<string, int>(wejscie.Split(" "), int.Parse);
 int daneZero = dane[0];
 int daneJeden = dane[1];
 int daneMod = dane[2];
-List<int> moduloList = new List<int>();
+List <int> moduloList = new List<int>();
 if (dane[0] < dane[1]) { 
     while (dane[0] < dane[1]-1)
     {
@@ -14,7 +13,7 @@ if (dane[0] < dane[1]) {
         if (dane[0] % dane[2] == 0)
         {
             moduloList.Add(dane[0]);
-            Console.Write($"{dane[0]}, ");
+          //  Console.Write($"{dane[0]}, ");
         }
    
     }
@@ -26,7 +25,7 @@ else if(dane[0] > dane[1]){
         if (dane[1] % dane[2] == 0)
         {
             moduloList.Add(dane[1]);
-            Console.Write($"{dane[1]}, ");
+          //  Console.Write($"{dane[1]}, ");
         }
    
     }
@@ -44,143 +43,27 @@ if (moduloCount==0)
 }
 
 else
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*{
-    if (dane[1] - dane[0] <= 11&& dane[0] - dane[1] <= 11)
+{
+    if (moduloCount <= 10) { 
+    for(int i = 0; i< moduloCount; i++)
     {
-
-    
-        if (dane[0] < dane[1])
-        { 
-            while (dane[0] < dane[1])
-            {
-                    dane[0]++;
-                    if (dane[0] % dane[2] == 0)
-                    {
-                        if (dane[0] + dane[2] == dane[1])
-                        {
-                           Console.Write($"{dane[0]}");
-                           break;
-                        
-                        }
-                        Console.Write($"{dane[0]}, ");
-                    }
-
-                
-            }
-        }   
-        else if (dane[1] < dane[0])
+        if(i==moduloCount-1)
         {
-            while (dane[1] < dane[0])
-            {
-                dane[1]++;
-                if (dane[1] % dane[2] == 0)
-                {
-                    if (dane[1] + dane[2] == dane[0])
-                    {
-                        Console.Write($"{dane[1]}");
-                        break;
-                    }
-                    Console.Write($"{dane[1]}, ");
-                }   
-            }
+            Console.Write($"{moduloList[i]}");
         }
+        else { 
+        Console.Write($"{ moduloList[i]}, ");
+            }
     }
-    else if(dane[0] < dane[1])
-    {
-        while (dane[0] < daneZero+3)
-        {
-            dane[0]++;
-            Console.Write($"{dane[0]}, ");
-
-        }
-        if (dane[0] == daneZero + 3)
-        {
-            Console.Write("..., ");
-            Console.WriteLine($"{dane[1]-2}, {dane[1]-1}");
-        }
     }
     else
     {
-        while (dane[1] < daneJeden+3)
-        {
-            dane[1]++;
-            Console.Write($"{dane[1]}, ");
 
-        }
-        if (dane[1] == daneJeden + 3)
-        {
-            Console.Write("..., ");
-            Console.WriteLine($"{dane[0]-2}, {dane[0]-1}");
-           }
-        }
-    
+    }
+}
 
-}*/
 #if DEBUG
+    Console.WriteLine();
     Console.WriteLine("Press enter to close...");
     Console.ReadLine();
 #endif
